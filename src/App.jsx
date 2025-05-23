9ebf49da3b02b964c0852404d64142f3b791ccea
votre_nom <laposta.enzo@gmail.com>
2025-05-23 11:40:29 +0200

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg"
+import "./App.css"
 
-      <main>
-      
-      </main>
-  
+
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17°</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" className="info-icon" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
